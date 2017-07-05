1d96b1132e31ef0ae63534802cb46f41862e9733
kolya-t <toporkov.kolya96@gmail.com>
2017-07-05 16:28:19 +0300

    Enabled 'property' and 'resource' repositories

--- src/main/java/org/teapot/backend/repository/meta/TeapotPropertyRepository.java
+++ src/main/java/org/teapot/backend/repository/meta/TeapotPropertyRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.teapot.backend.model.meta.TeapotProperty;
 
-@RepositoryRestResource(exported = false, path = "props", collectionResourceRel = "properties", itemResourceRel = "property")
+@RepositoryRestResource(path = "props", collectionResourceRel = "properties", itemResourceRel = "property")
 public interface TeapotPropertyRepository extends JpaRepository<TeapotProperty, Long> {
 
     TeapotProperty findByName(String name);
--- src/main/java/org/teapot/backend/repository/meta/TeapotResourceRepository.java
+++ src/main/java/org/teapot/backend/repository/meta/TeapotResourceRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.teapot.backend.model.meta.TeapotResource;
 
-@RepositoryRestResource(exported = false, path = "resources", collectionResourceRel = "resources", itemResourceRel = "resource")
+@RepositoryRestResource(path = "resources", collectionResourceRel = "resources", itemResourceRel = "resource")
 public interface TeapotResourceRepository extends JpaRepository<TeapotResource, Long> {
 
     TeapotResource findByName(String name);
