8291b7080ce491e7a0706b64710e773ec0030951
kolya-t <toporkov.kolya96@gmail.com>
2017-07-13 14:58:26 +0300

    'users' and 'organizations' joined to 'owners'

---
+++ src/main/java/org/teapot/backend/config/OwnerRelProvider.java
@@ -0,0 +1,27 @@
+package org.teapot.backend.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.hateoas.core.EvoInflectorRelProvider;
+import org.springframework.stereotype.Component;
+import org.teapot.backend.model.Owner;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class OwnerRelProvider extends EvoInflectorRelProvider {
+
+    @Override
+    public String getCollectionResourceRelFor(Class<?> type) {
+        return super.getCollectionResourceRelFor(Owner.class);
+    }
+
+    @Override
+    public String getItemResourceRelFor(Class<?> type) {
+        return super.getItemResourceRelFor(Owner.class);
+    }
+
+    @Override
+    public boolean supports(Class<?> delimiter) {
+        return Owner.class.isAssignableFrom(delimiter);
+    }
+}
