43def907c50c5c9d1c1734427999b1f96a04afc4
kolya-t <toporkov.kolya96@gmail.com>
2017-09-11 22:41:30 +0300

    Fixed bug when it was impossible to get access_token by refresh_token

--- src/main/java/org/teapot/backend/config/security/oauth2/AuthorizationServerConfig.java
+++ src/main/java/org/teapot/backend/config/security/oauth2/AuthorizationServerConfig.java
@@ -1,17 +1,28 @@
 package org.teapot.backend.config.security.oauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.teapot.backend.config.security.model.AuthenticatedRequestContext;
+import org.teapot.backend.model.user.User;
+import org.teapot.backend.repository.user.UserRepository;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -19,6 +30,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
         configurer.authenticationManager(authenticationManager);
+        configurer.userDetailsService(userDetailsService());
     }
 
     @Override
@@ -34,5 +46,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .resourceIds("resource");
+    }
+
+    @Bean
+    public DaoAuthenticationProvider getAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(User.PASSWORD_ENCODER);
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Autowired
+    public void authenticationManager(AuthenticationManagerBuilder builder) {
+        builder.authenticationProvider(getAuthenticationProvider());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userRepository.findByEmail(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("User '" + username + "' not found");
+            }
+
+            return new AuthenticatedRequestContext(user);
+        };
     }
 }
