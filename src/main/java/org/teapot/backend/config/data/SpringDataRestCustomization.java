9d860a0f5fd7b07f516838d0a9dac3e046ee297b
kolya-t <toporkov.kolya96@gmail.com>
2018-04-29 13:30:57 +0300

    expose all ids

--- src/main/java/org/teapot/backend/config/data/SpringDataRestCustomization.java
+++ src/main/java/org/teapot/backend/config/data/SpringDataRestCustomization.java
@@ -1,16 +1,25 @@
 package org.teapot.backend.config.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
 @Component
 @EnableJpaAuditing
 public class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter {
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.getCorsRegistry().addMapping("/**").allowedMethods("*");
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+                .map(Type::getJavaType).toArray(Class[]::new));
     }
 }
